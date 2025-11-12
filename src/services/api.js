fbd34814bd389936bc509141006a8dbfc805c341
Moshe Nahshon <117845188+moshenh01@users.noreply.github.com>
2025-11-12 09:45:23 +0200

    Change API_URL environment variable reference

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { isTokenExpired } from '../utils/tokenUtils';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.BACKEND_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
